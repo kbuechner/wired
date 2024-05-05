aefa641dc5a6dd29bd045f114082a1065f313734
Katherine Buechner <kbuechner@gmail.com>
2024-05-05 03:12:36 -0400

    basic display of some fields

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ fetch('/data/tarot-images.json')
     return body.json();
   })
   .then((body) => {
-    // do stuff with the data!!
-  });
+    const major = document.getElementById('major');
+    const minor = document.getElementById('minor');
+
+    Object.values(body.cards).forEach((card) => {
+      let isMajor = card.arcana === 'Major Arcana';
+      let text = isMajor ? card.Archetype : card.Affirmation;
+      let fortunes = card.fortune_telling;
+
+      const cardContainer = document.createElement('div');
+      cardContainer.setAttribute('id', card.name);
+
+      const fortuneContainer = document.createElement('div');
+      const archetypeElement = document.createElement('b');
+
+      const cardName = document.createElement('h3');
+      cardName.insertAdjacentText('afterbegin', card.name);
+
+      isMajor
+        ? major.insertAdjacentElement('beforeend', cardContainer)
+        : minor.insertAdjacentElement('beforeend', cardContainer);
+
+      archetypeElement.insertAdjacentText('beforeend', text);
+
+      fortunes.forEach((fortune) => {
+        const fortuneElement = document.createElement('div');
+        fortuneElement.append(fortune);
+        fortuneContainer.append(fortuneElement);
+      });
+
+      cardContainer.append(cardName, archetypeElement, fortuneContainer);
+    });  });
